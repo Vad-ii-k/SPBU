b043e8bfdeedf71662ce31111be2a6d853b729e8
vad-ii-k <sazanov7615@gmail.com>
2020-11-07 13:30:48 +0300

    Version 2.0 "Correction of suggested changes"

--- HW 3.1/Main.c
+++ HW 3.1/Main.c
@@ -21,7 +21,10 @@ int main()
     testCorrestSort();
     int array[SIZE];
     randArray(array, SIZE);
+    printf("Generated array: ");
+    printArray(array, SIZE);
     qsortModified(array, SIZE);
+    printf("Sorted array:    ");
     printArray(array, SIZE);
     return EXIT_SUCCESS;
 }
@@ -86,19 +89,15 @@ void swap(int* x, int* y)
 
 void randArray(int* array, const int size)
 {
-    printf("Generated array: ");
     srand(31);
     for (int i = 0; i < size; i++)
     {
         array[i] = rand() % 100;
-        printf("%2d ", array[i]);
     }
-    printf("\n");
 }
 
 void printArray(int* array, const int size)
 {
-    printf("Sorted array:    "); 
     for (int i = 0; i < size; i++)
     {
         printf("%2d ", array[i]);
@@ -108,11 +107,35 @@ void printArray(int* array, const int size)
 
 void testCorrestSort()
 {
-    int testArray[15] = { 36, 91,  0, 40, 80, 95, 35,  -3, 78, 10, 16, 22, 49, 51, 42 };
-    qsortModified(testArray, 15);
-    int testSortArray[15] = { -3,  0, 10, 16, 22, 35, 36, 40, 42, 49, 51, 78, 80, 91, 95 };
+    int testArray1[15] = { 36, 91,  0, 40, 80, 95, 35,  -3, 78, 10, 16, 22, 49, 51, 42 };
+    qsortModified(testArray1, 15);
+    int testSortArray1[15] = { -3,  0, 10, 16, 22, 35, 36, 40, 42, 49, 51, 78, 80, 91, 95 };
     for (int i = 0; i < 15; i++)
     {
-        assert(testArray[i] == testSortArray[i]);
+        assert(testArray1[i] == testSortArray1[i]);
+    }
+
+    int testArray2[1] = { 100 };
+    qsortModified(testArray2, 1);
+    int testSortArray2[1] = { 100 };
+    for (int i = 0; i < 1; i++)
+    {
+        assert(testArray2[i] == testSortArray2[i]);
+    }
+
+    int testArray3[11] = { 40, 80, 95, 35,  -3, 78, 10, 16, 22, 49, 51 };
+    qsortModified(testArray3, -10);
+    int testSortArray3[11] = { 40, 80, 95, 35,  -3, 78, 10, 16, 22, 49, 51 };
+    for (int i = 0; i < 11; i++)
+    {
+        assert(testArray3[i] == testSortArray3[i]);
+    }
+
+    int testArray4[15] = { 25, 25, 25, 25, -25, 25, 25, 25, 25, 25, 25, 25, 15, 25, 25 };
+    qsortModified(testArray4, 15);
+    int testSortArray4[15] = { -25, 15, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25 };
+    for (int i = 0; i < 15; i++)
+    {
+        assert(testArray4[i] == testSortArray4[i]);
     }
 }
