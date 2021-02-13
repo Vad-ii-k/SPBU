f9b59790c9c55fd19cd10a9983b080161ddbab9e
vad-ii-k <sazanov7615@gmail.com>
2021-02-13 15:39:09 +0300

    Final

--- HW 4.3/Main.c
+++ HW 4.3/Main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 typedef struct
 {
@@ -16,6 +17,10 @@ Contact createContact(char* name, char* number)
 	Contact person;
 	person.name = malloc(sizeof(char) * (strlen(name) + 1));
 	person.number = malloc(sizeof(char) * (strlen(number) + 1));
+	if (person.name == NULL || person.number == NULL)
+	{
+		exit(1);
+	}
 	if (person.name && person.number)
 	{
 		strcpy(person.name, name);
@@ -24,33 +29,21 @@ Contact createContact(char* name, char* number)
 	return person;
 }
 
-int inputComand()
-{
-	printf("\nType the command: ");
-	int num = 0;
-	scanf_s("%d", &num);
-	return num;
-}
+int inputComand();
+
+void printMenu();
 
 void savePhoneNumber(Contact phone, int phonesCount);
 
+void printContacts(Contact* phones, int phonesCount);
+
 int loadContacts(Contact phones[100]);
 
-void addAnEntry(int* phonesCount, Contact* phones);
+void addAnEntry(Contact* phones, int* phonesCount);
 
-void searchByName(int* phonesCount, Contact* phones);
+void searchByName(Contact* phones, int phonesCount);
 
-void searchByNumber(int* phonesCount, Contact* phones);
-
-void printMenu()
-{
-	printf("Menu:\n");
-	printf("0 - exit\n");
-	printf("1 - add an entry(name and phone number)\n");
-	printf("2 - print all available entries\n");
-	printf("3 - find phone by name\n");
-	printf("4 - find name by phone\n");
-}
+void searchByNumber(Contact* phones, int phonesCount);
 
 int main()
 {
@@ -71,28 +64,22 @@ int main()
 		}
 		case 1:
 		{
-			addAnEntry(&phonesCount, phones);
+			addAnEntry(phones, &phonesCount);
 			break;
 		}
 		case 2:
 		{
-			printf("------------------\n");
-			for (int i = 0; i < phonesCount; i++)
-			{
-				printf("Name: %s\n", phones[i].name);
-				printf("Phone: %s\n", phones[i].number);
-				printf("------------------\n");
-			}
+			printContacts(phones, phonesCount);
 			break;
 		}
 		case 3:
 		{
-			searchByName(&phonesCount, phones);
+			searchByName(phones, &phonesCount);
 			break;
 		}
 		case 4:
 		{
-			searchByNumber(&phonesCount, phones);
+			searchByNumber(phones, &phonesCount);
 			break;
 		}
 		default:
@@ -103,12 +90,46 @@ int main()
 	}
 }
 
+void printMenu()
+{
+	printf("Menu:\n");
+	printf("0 - exit\n");
+	printf("1 - add an entry(name and phone number)\n");
+	printf("2 - print all available entries\n");
+	printf("3 - find phone by name\n");
+	printf("4 - find name by phone\n");
+}
+
+int inputComand()
+{
+	printf("\nType the command: ");
+	char cmd[100];
+	gets_s(cmd, 99);
+	if (isdigit((unsigned char)cmd[0]))
+	{
+		return cmd[0] - '0';
+	}
+	else
+		return 5;
+}
+
+void printContacts(Contact* phones, int phonesCount)
+{
+	printf("------------------\n");
+	for (int i = 0; i < phonesCount; i++)
+	{
+		printf("Name: %s\n", phones[i].name);
+		printf("Phone: %s\n", phones[i].number);
+		printf("------------------\n");
+	}
+}
+
 void savePhoneNumber(Contact phone, int phonesCount)
 {
 	FILE* file = fopen("DATA.txt", "a");
 	if (file == NULL)
 	{
-		return;
+		exit(EXIT_FAILURE);
 	}
 
 	if (phonesCount > 0)
@@ -119,6 +140,7 @@ void savePhoneNumber(Contact phone, int phonesCount)
 	fputs("\n", file);
 	fputs(phone.number, file);
 	fclose(file);
+	printf("Saved\n");
 }
 
 int loadContacts(Contact phones[100])
@@ -127,7 +149,7 @@ int loadContacts(Contact phones[100])
 	if (file == NULL)
 	{
 		printf("File not found!");
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	int phoneCount = 0;
@@ -144,6 +166,7 @@ int loadContacts(Contact phones[100])
 		if (read <= 0)
 		{
 			printf("Didn't read anything!");
+			exit(EXIT_FAILURE);
 		}
 		phones[phoneCount] = createContact(name, number);
 		phoneCount++;
@@ -152,12 +175,12 @@ int loadContacts(Contact phones[100])
 	return phoneCount;
 }
 
-void addAnEntry(int* phonesCount, Contact* phones)
+void addAnEntry(Contact* phones, int* phonesCount)
 {
 	if (*phonesCount == 99)
 	{
 		printf("Contact limit exceeded!\n");
-		return;
+		exit(EXIT_FAILURE);
 	}
 
 	char name[50];
@@ -165,7 +188,7 @@ void addAnEntry(int* phonesCount, Contact* phones)
 	if (!scanf("%s", name))
 	{
 		printf("Error!");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	char number[50];
@@ -173,7 +196,7 @@ void addAnEntry(int* phonesCount, Contact* phones)
 	if (!scanf("%s", number))
 	{
 		printf("Error!");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	Contact person = createContact(name, number);
@@ -181,20 +204,19 @@ void addAnEntry(int* phonesCount, Contact* phones)
 	(*phonesCount)++;
 
 	savePhoneNumber(person, *phonesCount);
-	printf("Conserved\n");
 }
 
-void searchByName(int* phonesCount, Contact* phones)
+void searchByName(Contact* phones, int phonesCount)
 {
 	char name[50] = { 0 };
 	printf("Enter a name to search for: ");
 	if (!scanf("%s", name))
 	{
 		printf("Error!");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
-	for (int i = 0; i < *phonesCount; i++)
+	for (int i = 0; i < phonesCount; i++)
 	{
 		if (!strcmp(phones[i].name, name))
 		{
@@ -204,24 +226,24 @@ void searchByName(int* phonesCount, Contact* phones)
 			printf("------------------\n");
 			break;
 		}
-		if (i == *phonesCount - 1)
+		if (i == phonesCount - 1)
 		{
 			printf("Contact not found\n");
 		}
 	}
 }
 
-void searchByNumber(int* phonesCount, Contact* phones)
+void searchByNumber(Contact* phones, int phonesCount)
 {
 	char number[50] = { 0 };
 	printf("Enter the number to search for: ");
 	if (!scanf("%s", number))
 	{
 		printf("Error!");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
-	for (int i = 0; i < *phonesCount; i++)
+	for (int i = 0; i < phonesCount; i++)
 	{
 		if (!strcmp(phones[i].number, number))
 		{
@@ -231,7 +253,7 @@ void searchByNumber(int* phonesCount, Contact* phones)
 			printf("------------------\n");
 			break;
 		}
-		if (i == *phonesCount - 1)
+		if (i == phonesCount - 1)
 		{
 			printf("Contact not found\n");
 		}
