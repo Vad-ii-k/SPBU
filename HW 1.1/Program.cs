32aa291979636467d32f3ebd064249632b65fe05
vad-ii-k <sazanov7615@gmail.com>
2021-02-20 16:24:56 +0300

    Version 2.0 "Correction of suggested changes"

--- HW 1.1/Program.cs
+++ HW 1.1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace HW_1._1
 {
@@ -7,10 +6,14 @@ namespace HW_1._1
     {
         static void Main(string[] args)
         {
-            Test();
+            if (!Test())
+            {
+                Console.WriteLine("Tests failed!");
+            }
+
             Console.WriteLine($"Enter array elements:");
-            string[] nums = Console.ReadLine().Split(' ');
-            int[] array = new int[nums.Length];
+            string[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var array = new int[nums.Length];
             for (int i = 0; i < nums.Length; i++)
             {
                 array[i] = int.Parse(nums[i]);
@@ -27,24 +30,63 @@ namespace HW_1._1
         static void BubbleSort(int[] sortArray)
         {
             for (int i = 0; i < sortArray.Length - 1; i++)
+            {
                 for (int j = 0; j < sortArray.Length - 1 - i; j++)
+                {
                     if (sortArray[j] > sortArray[j + 1])
                     {
                         int temp = sortArray[j];
                         sortArray[j] = sortArray[j + 1];
                         sortArray[j + 1] = temp;
                     }
+                }
+            }
         }
 
-        static void Test()
+        static bool Test()
         {
+            bool tests = true;
             int[] testArray = { 36, 91, 0, 40, 80, 95, 35, -3, 78, 10, 16, 22, 49, 51, 42 };
             BubbleSort(testArray);
             int[] testSortArray = { -3, 0, 10, 16, 22, 35, 36, 40, 42, 49, 51, 78, 80, 91, 95 };
             for (int i = 0; i < testArray.Length; i++)
             {
-                Debug.Assert(testArray[i] == testSortArray[i]);
+                if (testArray[i] != testSortArray[i])
+                {
+                    tests = false;
+                }
             }
+            int[] testArrayOneElement = { 100 };
+            BubbleSort(testArrayOneElement);
+            int[] testSortArrayOneElement = { 100 };
+            for (int i = 0; i < testArrayOneElement.Length; i++)
+            {
+                if (testArrayOneElement[i] != testSortArrayOneElement[i])
+                {
+                    tests = false;
+                }
+            }
+            int[] testArrayEmpty = { };
+            BubbleSort(testArrayEmpty);
+            int[] testSortArrayOEmtpy = { };
+            for (int i = 0; i < testArrayEmpty.Length; i++)
+            {
+                if (testArrayEmpty[i] != testSortArrayOEmtpy[i])
+                {
+                    tests = false;
+                }
+            }
+            int[] testArraySorted = { 1, 20, 400, 420, 1000 };
+            BubbleSort(testArraySorted);
+            int[] testSortArraySorted = { 1, 20, 400, 420, 1000 };
+            for (int i = 0; i < testArraySorted.Length; i++)
+            {
+                if (testArraySorted[i] != testSortArraySorted[i])
+                {
+                    tests = false;
+                }
+            }
+            return tests;
         }
     }
 }
